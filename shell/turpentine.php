872b76908af42b4685f1becb2ae53d95815a352c
Claudio Fior @ abbrevia <caiofior@gmail.com>
2014-09-12 17:52:20 +0200

    Updated some graphic

--- shell/turpentine.php
+++ shell/turpentine.php
@@ -3,7 +3,7 @@
 require_once  __DIR__.'/../app/Mage.php';
  // if your are not root folder then write the proper path like publichtml/magento/app/Mage.php
 
-Mage::app('default');
+Mage::app('storebaby');
 Mage::getModel( 'turpentine/varnish_admin' )->applyConfig();
 Mage::getModel( 'turpentine/varnish_admin' )->flushAll();
 foreach( Mage::helper( 'turpentine/varnish' )->getSockets() as $socket ) {
